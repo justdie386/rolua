49e5470936138854d8a3c8cc3884ba10ad2c550a
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2023-11-15 19:01:02 -0500

    Update groupUtils.lua

--- examples/groupUtils.lua
+++ examples/groupUtils.lua
@@ -5,6 +5,8 @@ rolua.xcsrf.token()
 
 local groupInfo = rolua.group.getGroup("group id") --you must have the permission to manage the fund's group to acces this information
 local groupTransaction, event = rolua.group.getGroupTransaction("group id") --you must have the permission to manage the fund's group to acces this information
+p(groupInfo)
+p(groupTransaction)
 local robuxAmount = rolua.group.getGroupRobux("group id") --gets the current amount of robux on the group, once again requires the funds management perms
 rolua.group.OnGroupTransaction("group id", event) --listens to when a new purchase is made within the roblox group and its owned games
 event:on("PurchaseBought", function(data)
