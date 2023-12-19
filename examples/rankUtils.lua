8fb8600cd7b5b2dd5d7840dd1f01b50e4fd10db1
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2023-12-19 12:43:00 -0500

    Update rankUtils.lua
    Made it easier to understand

--- examples/rankUtils.lua
+++ examples/rankUtils.lua
@@ -1,7 +1,10 @@
 local rolua = require("rolua")
 local cookie = "insert your cookie here"
+local groupID = "whatever"
+local rankName = "whatever"
+local userID = "userID"
 rolua.cookie.setCookie(cookie)
 rolua.xcsrf.token()
 
-rolua.rank.getRankID("group id", "rank name") --useful to get the rank id idk
-rolua.rank.setRank("user's id as either a string or a number","group's id as a number","new rank's name") --will return an error if you try to set the user's rank to something he already is
+rolua.rank.getRankID(groupID, rankName) --Its the only way to get the rank ID to then use it in setRank
+rolua.rank.setRank(userID, groupID, rankName) --will return an error if you try to set the user's rank to something he already is
