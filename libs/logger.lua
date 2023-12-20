c905df1fa9292f84388f1b84e8620a5316681122
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2023-12-20 12:28:27 -0500

    Fixed indentation with logger.lua
    Indented a bit

--- libs/logger.lua
+++ libs/logger.lua
@@ -5,14 +5,13 @@ function logger.info(message)
     local timestamp = os.date("%Y-%m-%d %H:%M:%S")
     local green = "\27[1;32m"  -- ANSI escape code for green text
     local reset = "\27[0m"   -- ANSI escape code to reset text color
-
     print(timestamp .. " | " .. green .. "[INFO]" .. reset .. "    | " .. message)
 end
 function logger.error(message)
 	local RED_BOLD = "\27[1;31m"   
 	local RESET = "\27[0m"
-    local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-    print(timestamp .. " | " .. RED_BOLD .. "[INFO]" .. RESET .. "    | " .. message)
+    	local timestamp = os.date("%Y-%m-%d %H:%M:%S")
+    	print(timestamp .. " | " .. RED_BOLD .. "[INFO]" .. RESET .. "    | " .. message)
 end
 
 return logger
