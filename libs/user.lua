4e07b0b9a275d9ac2856c1c072d2243c4e9c881a
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2024-04-10 19:55:16 -0400

    Update user.lua
    fixed the indent

--- libs/user.lua
+++ libs/user.lua
@@ -1,14 +1,15 @@
 local HTTPRequest = require("http.lua")
 local logger = require("logger.lua")
 local user = {}
+
 function user.getUser(name)
-local headers = {{"accept", "application/json"}} --uses its own header because it doesn't need the cookie
-		local success, res, body = HTTPRequest:callRobloxAPI("GET", "https://users.roblox.com/v1/users/"..name, headers)
+	local headers = {{"accept", "application/json"}} --uses its own header because it doesn't need the cookie
+	local success, res, body = HTTPRequest:callRobloxAPI("GET", "https://users.roblox.com/v1/users/"..name, headers)
 	if not success then
-        print("failed to get group: "..res.reason)
-    else
-        return body
-    end
+		print("failed to get group: "..res.reason)
+    	else
+        	return body
+    	end
 end
 
 return user
