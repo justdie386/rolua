05a08bfff2390c408faf29063125d357b7fe4eb7
Why do you care? <79466202+justdie386@users.noreply.github.com>
2025-12-05 08:21:05 -0500

    Update groupUtils.lua

--- examples/groupUtils.lua
+++ examples/groupUtils.lua
@@ -1,12 +1,15 @@
 local rolua = require("rolua")
 local cookie = "insert your cookie here"
+
 rolua.cookie.setCookie(cookie)
 rolua.xcsrf.token()
+
 local groupID = "whatever"
 local groupInfo = rolua.group.getGroup(groupID) --will return info about the group
 local groupTransaction, event = rolua.group.getGroupTransaction(groupID) --you must have the permission to manage the fund's group to acces this information
 local robuxAmount = rolua.group.getGroupRobux(groupID) --gets the current amount of robux on the group, once again requires the funds management perms
 rolua.group.OnGroupTransaction(groupID, event) --listens to when a new purchase is made within the roblox group and its owned games
+
 event:on("PurchaseBought", function(data)
     --print out the data to see what you need
 end)
